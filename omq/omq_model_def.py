0a8d027ab8b6997f71e79407599d860b89111b88
ivp <ivan.panov>
2019-03-26 22:47:26 +0100

    dev snapshot

--- omq/omq_model_def.py
+++ omq/omq_model_def.py
@@ -5,13 +5,10 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
 
-import prepare_omq
-
-X = prepare_omq.X
-y = prepare_omq.y
-
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
-model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+def get_model(X, y):
+    model = Sequential()
+    model.add(LSTM(512, input_shape=(X.shape[1], X.shape[2])))
+    # model.add(Dropout(0.2))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    return model
